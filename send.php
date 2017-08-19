c6d642aeecd44773e017ca85c6e30b2bcbace451
vitekf <vita.brno@centrum.cz>
2017-08-19 14:29:24 +0200

    Add files via upload

---
+++ send.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <form action="./" method="post">
+      
+      <table border="0">
+      <tr><td align="right">Name:</td><td><input type="text" name="secret" size="10" value="ItsMe"></td></tr>
+      <tr><td align="right">N (50.000):</td><td><input type="text" name="latitude" size="10" value=""></td></tr>
+      <tr><td align="right">E (16.000):</td><td><input type="text" name="longitude" size="10" value= ""></td></tr>
+      <tr><td align="right">elevation:</td><td><input type="text" name="altitude" size="10" value= "1"></td></tr>
+      <tr><td align="center" colspan="2"><input type="submit" value="Odeslat" style="width:100%"></td></tr>
+      </table>
+      
+      
+      
+    </form> 
+  </body>
+</html>
